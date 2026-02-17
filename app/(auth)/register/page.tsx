2b440b819df540d5177c947b49b4b12c8c5e2389
Lozaashenafi <134071796+Lozaashenafi@users.noreply.github.com>
2026-02-17 12:14:49 +0300

    first commit

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import AuthPage from "@/components/home/AuthPage";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <AuthPage type="sign-up" />
+    </>
+  );
+}
+
+export default page;
---
+++ app/(site)/explore/page.tsx
@@ -0,0 +1,12 @@
+import ExplorePage from "@/components/explore/ExplorePage";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <ExplorePage />
+    </>
+  );
+}
+
+export default page;

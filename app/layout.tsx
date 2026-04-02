670dc6833634b2db811f46981867016f1d665380
Lozaashenafi <lozaashenafi@gmail.com>
2026-04-02 14:03:23 +0300

    fix: update metadata description and image URLs for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,13 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   metadataBase: new URL("https://bookpulse.lozi.me"),
   title: {
     default: "BookPulse | Turn the page, together",
     template: "%s | BookPulse",
   },
-  description:
-    "BookPulse is a social reading platform that connects book lovers, allowing them to share their reading experiences, discover new books, and engage in meaningful discussions. Join our community and turn the page together!",
+  description: "BookPulse is a social reading platform...",
   openGraph: {
     title: "BookPulse | Turn the page, together",
     description: "The social reading platform for book lovers.",
@@ -31,17 +29,20 @@ export const metadata: Metadata = {
     siteName: "BookPulse",
     images: [
       {
-        url: "/image/home.png", // Path relative to the public folder
+        url: "https://bookpulse.lozi.me/image/home.png", // Full URL
         width: 1200,
         height: 630,
-        alt: "BookPulse Preview Image",
       },
     ],
     locale: "en_US",
     type: "website",
   },
- 
+  twitter: {
+    card: "summary_large_image",
+    images: ["https://bookpulse.lozi.me/image/home.png"], // Full URL
+  },
 };
+
 
 // app/layout.tsx
 export default function RootLayout({
